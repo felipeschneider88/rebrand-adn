a0882653a3f124b07d931b36e925b64bbae01636
felipeschneider88 <felipes88@gmail.com>
2023-05-16 19:00:10 -0300

    add footer and header in all pages

--- app/about/page.js
+++ app/about/page.js
@@ -3,7 +3,11 @@ import styles from '../page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
+    <HeaderComponent></HeaderComponent>            
+      <br /> 
         <p>Here goes the about secction</p>
+      <br />            
+      <FooterComponent></FooterComponent>   
     </main>
   )
 }
